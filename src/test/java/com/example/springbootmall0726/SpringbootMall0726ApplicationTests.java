e2f7704288860c0e5f47fd3c828f06479638b68a
黃鼎文 <aa6413779@hotmail.com.tw>
2023-08-10 16:30:14 +0800

    maven package test and add Dockerfile for deployment

--- src/test/java/com/example/springbootmall0726/SpringbootMall0726ApplicationTests.java
+++ src/test/java/com/example/springbootmall0726/SpringbootMall0726ApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.springbootmall0726;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringbootMall0726ApplicationTests.class)
 class SpringbootMall0726ApplicationTests {
 
     @Test
