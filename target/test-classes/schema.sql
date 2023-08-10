404f45814ce5841fb408c9847fb6844fa5a723ae
黃鼎文 <aa6413779@hotmail.com.tw>
2023-08-10 23:24:35 +0800

    re-package for deployment

---
+++ target/test-classes/schema.sql
@@ -0,0 +1,40 @@
+CREATE TABLE IF NOT EXISTS product
+(
+    product_id         INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    product_name       VARCHAR(128) NOT NULL,
+    category           VARCHAR(32)  NOT NULL,
+    image_url          VARCHAR(256) NOT NULL,
+    price              INT          NOT NULL,
+    stock              INT          NOT NULL,
+    description        VARCHAR(1024),
+    created_date       TIMESTAMP    NOT NULL,
+    last_modified_date TIMESTAMP    NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `user`
+(
+    user_id            INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email              VARCHAR(256) NOT NULL UNIQUE,
+    password           VARCHAR(256) NOT NULL,
+    created_date       TIMESTAMP    NOT NULL,
+    last_modified_date TIMESTAMP    NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS `order`
+(
+    order_id           INT       NOT NULL Primary Key AUTO_INCREMENT,
+    user_id            INT       NOT NULL,
+    total_amount       DECIMAL       NOT NULL,
+    created_date       TIMESTAMP NOT NULL,
+    last_modified_date TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS order_item
+(
+    order_item_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    order_id      INT NOT NULL,
+    product_id    INT NOT NULL,
+    quantity      INT NOT NULL,
+    amount        DECIMAL NOT NULL
+);
